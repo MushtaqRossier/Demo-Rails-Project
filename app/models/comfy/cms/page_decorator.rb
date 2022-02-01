eac119df3b780008b52b2953f50d630cc4e20cb2
Mushtaq Rossier <mushtaqmango16@gmail.com>
2022-02-01 09:24:27 +0200

    Fixed environment issues

--- app/models/comfy/cms/page_decorator.rb
+++ app/models/comfy/cms/page_decorator.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
  
 Comfy::Cms::Page.class_eval do
-    belongs_to :product, class_name: 'Spree::Product'
+    belongs_to :product, class_name: 'Spree::Product', optional: true
   end
